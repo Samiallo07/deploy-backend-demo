b860aecd0caa36182d4d00d58ca8b4529849be65
Samiallo07 <nusratullosamiallo@gmail.com>
2026-07-13 13:41:05 -0400

    Prepare backend for Render deployment

--- db.js
+++ db.js
@@ -1,15 +1,26 @@
-const { Sequelize, DataTypes } = require('sequelize')
+const { Sequelize, DataTypes } = require("sequelize");
 
-const DB_URL = process.env.DATABASE_URL || 'postgres://localhost:5432/ttp'
+const DB_URL =
+  process.env.DATABASE_URL || "postgres://localhost:5432/ttp";
 
-const db = new Sequelize(DB_URL)
+const db = new Sequelize(DB_URL, {
+  dialect: "postgres",
+  dialectOptions: process.env.DATABASE_URL
+    ? {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      }
+    : {},
+});
 
 // models
-const UserModel = db.define('user', {
-  name: DataTypes.STRING
-})
+const UserModel = db.define("user", {
+  name: DataTypes.STRING,
+});
 
 module.exports = {
-  db, 
-  UserModel
-}
+  db,
+  UserModel,
+};
